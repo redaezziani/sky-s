ec466506caf7a9614c3460f418bc5d48aedb8140
redaezzaini <redaezziani.dev@gmail.com>
2025-09-22 10:36:05 +0100

    fix:fix the sidebar links to have also a active bool

--- frontend/src/app/dashboard/order-items/page.tsx
+++ frontend/src/app/dashboard/order-items/page.tsx
@@ -4,7 +4,8 @@ import { useEffect } from "react";
 import { EnhancedOrderItemsTable } from "@/components/order-item/enhabced-order-items-table";
 import { useOrderItemsStore } from "@/stores/order-items-store";
 
-
+import { useLocale } from "@/components/local-lang-swither"; // your LocaleProvider hook
+import { getMessages } from "@/lib/locale"; 
 export default function OrdersItemsPage() {
   const { fetchItems } = useOrderItemsStore();
 
@@ -13,14 +14,15 @@ export default function OrdersItemsPage() {
   }
   , [fetchItems]);
 
+    const { locale } = useLocale();
+    const t = getMessages(locale);
+
   return (
     <section className="flex flex-col gap-4 w-full px-6 py-4">
       <div className="flex justify-between items-center">
         <div>
-          <h1 className="text-2xl font-semibold">Orders Items Management</h1>
-          <p className="text-muted-foreground">
-            Manage customer orders, payments, and deliveries
-          </p>
+          <h1 className="text-2xl font-semibold">{t.pages.orders.title}</h1>
+          <p className="text-muted-foreground">{t.pages.orders.description}</p>
         </div>
       </div>
       <EnhancedOrderItemsTable />
--- frontend/src/components/nav-main.tsx
+++ frontend/src/components/nav-main.tsx
@@ -1,70 +1,58 @@
-"use client"
+"use client";
 
-import Link from "next/link"
-import { IconCirclePlusFilled, IconMail, type Icon } from "@tabler/icons-react"
+import Link from "next/link";
+import { type Icon } from "@tabler/icons-react";
 
-import { Button } from "@/components/ui/button"
 import {
   SidebarGroup,
   SidebarGroupContent,
   SidebarMenu,
   SidebarMenuButton,
   SidebarMenuItem,
-} from "@/components/ui/sidebar"
-import { usePath } from "@/hooks/use-path"
+} from "@/components/ui/sidebar";
+import { usePath } from "@/hooks/use-path";
 
 export function NavMain({
   items,
 }: {
   items: {
-    title: string
-    url: string
-    icon?: Icon
-  }[]
+    title: string;
+    url: string;
+    icon?: Icon;
+    active: boolean;
+  }[];
 }) {
   return (
     <SidebarGroup>
       <SidebarGroupContent className="flex flex-col gap-2">
-        {/* <SidebarMenu>
-          <SidebarMenuItem className="flex items-center gap-2">
-            <SidebarMenuButton
-              tooltip="Quick Create"
-              className="bg-primary text-primary-foreground hover:bg-primary/90 hover:text-primary-foreground active:bg-primary/90 active:text-primary-foreground min-w-8 duration-200 ease-linear"
-            >
-              <IconCirclePlusFilled />
-              <span>Quick Create</span>
-            </SidebarMenuButton>
-            <Button
-              size="icon"
-              className="size-8 group-data-[collapsible=icon]:opacity-0 bg-transparent"
-              variant="outline"
-            >
-              <IconMail />
-              <span className="sr-only">Inbox</span>
-            </Button>
-          </SidebarMenuItem>
-        </SidebarMenu> */}
         <SidebarMenu>
           {items.map((item) => {
             const isActive = usePath(item.url);
-            
+
             return (
               <SidebarMenuItem key={item.title}>
-                <SidebarMenuButton 
-                  tooltip={item.title} 
+                <SidebarMenuButton
+                  tooltip={item.title}
                   asChild
-                  className={isActive ? "bg-primary text-primary-foreground hover:bg-primary/90 hover:text-primary-foreground active:bg-primary/90 active:text-primary-foreground duration-200 ease-linear" : ""}
+                  className={
+                    isActive
+                      ? "bg-primary text-primary-foreground hover:bg-primary/90 hover:text-primary-foreground active:bg-primary/90 active:text-primary-foreground duration-200 ease-linear"
+                      : ""
+                  }
                 >
-                  <Link href={item.url}>
+                  <Link
+                    aria-disabled={!item.active} // this is mean the link is disabled if active is false
+                    href={item.url}
+                  >
                     {item.icon && <item.icon />}
                     <span>{item.title}</span>
                   </Link>
                 </SidebarMenuButton>
               </SidebarMenuItem>
-            )
+            );
           })}
         </SidebarMenu>
       </SidebarGroupContent>
     </SidebarGroup>
-  )
+  );
 }
